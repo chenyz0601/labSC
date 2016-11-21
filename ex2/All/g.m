dadf8341d20a7d992fd77830df2f2d7f378eee3c
a-parida12 <abhijeet.parida@tum.de>
2016-11-21 11:37:42 +0100

    Update g.m

--- ex2/All/g.m
+++ ex2/All/g.m
@@ -1,11 +1,11 @@
 function [p_t  ] = g(dt, t_end)
 
-%F defination of the differential equation
+%G defination of the solution of the differential equation
 
-%   This function is used to define the differential equation that needs to
-%   be solved and has to be defined in the format y'=f(y)
+%   This function is used to define the solution of the differential equation that needs to
+%   be solved 
 
-p_t= zeros(1,t_end/dt);%preallocation to prevent copying again and again while increasing size
+p_t= zeros(1,t_end/dt);%preallocation to prevent copying again and again while increasing size(increase speed)
 
 for i=1:((t_end/dt+1))
 p_t(i) = 10./(1+9*exp(-(i-1)*dt)); % Please define f(y) as y_dy= f(y)
