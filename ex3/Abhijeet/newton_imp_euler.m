009a8dbb7ff272106f722892b155d6249e79e620
a-parida12 <abhijeet.parida@tum.de>
2016-12-10 23:04:12 +0100

    orz

--- ex3/Abhijeet/newton_imp_euler.m
+++ ex3/Abhijeet/newton_imp_euler.m
@@ -4,14 +4,14 @@ function [ iter1] = newton_imp_euler( y_n,dt )
 
 
 eror=1;
-iter1=0;
+iter1=y_n;
 
  while (abs(eror)> 10^-4)
      %x_n+1=x_n - G(X)/G'(X)
      iter2= iter1-(10*iter1-10*y_n-dt*(70*iter1-7*(iter1)^2))/(10-dt*(56-14*y_n));
-          
+      
      %eror to check how close to 0 is G(X)
-     eror = iter2-y_n-dt*(f(iter2));
+     eror = iter2-iter1;
           
      iter1=iter2;
  end
