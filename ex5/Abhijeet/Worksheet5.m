20d16cc0d9ba4ca21ff643157900b58b7c0217f8
a-parida12 <abhijeet.parida@tum.de>
2017-01-21 17:42:18 +0100

    wtf

--- ex5/Abhijeet/Worksheet5.m
+++ ex5/Abhijeet/Worksheet5.m
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 clc;
-clear
+clear;
 
 % Boundary of the PDE 
 x_l=0;
@@ -41,48 +40,4 @@ tic
     toc
 
 end
-=======
-clc;
-clear
-
-% Boundary of the PDE 
-x_l=0;
-x_r= 1;
-
-y_d=0;
-y_u= 1;
-
-
-global j;
-j=1;
-
-N =[3,7,15,31]; % number of division for x
-dt_all=[1/64, 1/128, 1/256, 1/512, 1/1024, 1/2048, 1/4096];
-tic;
-for i=1:size(N,2)
-Nx=N(i);
-Ny = Nx;
-dx = (x_r-x_l) / (Nx+1);
-
-%init conditions
-Temp=ones(Ny*Ny,1);
-T= zeros(Nx+2,Nx+2);
-        for s=0:Nx-1
-            T(2:Nx+1,s+2)=Temp(s*Nx+1:(s+1)*Nx,1);
-        end
-
-for l=1:size(dt_all,2)
-    dt=dt_all(l);
-    tic
-    %explicit Euler solver
-    explicit_euler(T,dt,Nx,dx);
-     toc 
-end
-tic
- %implicit euler Solver
-    implicit_euler(T,dt_all(1),Nx,dx);
-    toc
-
-end
->>>>>>> 993df3cf6253d8ec7a428282feb40ea5432b3bd1
 toc;
--- ex5/Abhijeet/explicit_euler.m
+++ ex5/Abhijeet/explicit_euler.m
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 function [] = explicit_euler(T,dt,Nx,dx)
 %EXPLICIT_EULER Summary of this function goes here
 %   Detailed explanation goes here
-
-
 
 A=dt/(dx*dx);
 
@@ -24,30 +22,3 @@ end
 
 end
 
-=======
-function [] = explicit_euler(T,dt,Nx,dx)
-%EXPLICIT_EULER Summary of this function goes here
-%   Detailed explanation goes here
-
-
-
-A=dt/(dx*dx);
-
-for i=1:(0.5/dt)
-    %Defination of Explicit Euler method
-    for x=2:Nx+1
-            for y=2:Nx+1
-                T(x,y)=T(x,y)+A*(T(x-1,y)+T(x+1,y)+T(x,y+1)+T(x,y-1)-4*T(x,y));
-            end
-    end
-    
-    if i== (0.5/dt) ||i== (0.375/dt) || i== (.25/dt)|| i== (0.125/dt) 
-        str1=sprintf('Explicit Euler t=%f',i*dt);
-        plotter(T,Nx, dt, i,str1);   
-    end
-end
-
-
-end
-
->>>>>>> 993df3cf6253d8ec7a428282feb40ea5432b3bd1
